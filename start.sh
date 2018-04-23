9a3d27da786aa850e55370a7b151fa1668cc8562
Vtyurin, Maxim <maxim.vtyurin@accenture.com>
2018-04-23 22:51:35 +0300

    Update to non-root user. Set version 0.3

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 IP=`ip -4 a show dev eth0| grep inet | cut '-d/' -f1 | awk '{print $2}'`
-
 echo Current IP: $IP
 
-if [[ ! -d /var/www/mynewwiki ]]; then
-    tiddlywiki /var/www/mynewwiki --init server
+wikidir=${WIKI_HOME}/mynewwiki
+if [[ ! -d ${wikidir} ]]; then
+    mkdir -p ${wikidir}
+    tiddlywiki ${wikidir} --init server
 fi    
-tiddlywiki /var/www/mynewwiki --server 8086 '' '' '' '' '' $IP
+tiddlywiki ${wikidir} --server 8086 '' '' '' '' '' $IP
