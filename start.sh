6c49f0923563efe17b1707812be2242022dcac02
vtyurinmn <e-mercury@mail.ru>
2018-06-12 23:08:29 +0200

    Fix wikidir

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 IP=`ip -4 a show dev eth0| grep inet | cut '-d/' -f1 | awk '{print $2}'`
 echo Current IP: $IP
 
-wikidir=${WIKI_HOME}/mynewwiki
+wikidir=${WIKI_HOME}/mynewwiki/wikidata
 if [[ ! -d ${wikidir} ]]; then
     mkdir -p ${wikidir}
     tiddlywiki ${wikidir} --init server
